47e9c1569923dc15c46c0eb5d0a8398c7fabce88
Luis Morneno <morenoluise535@gmail.com>
2018-12-09 13:40:16 -0800

    added mysql schema and seeds/ added server

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS `burgers_db`;
+CREATE DATABASE `burgers_db`;
+
+use burgers_db;
+
+create table burgers(
+    id int auto_increment,
+    burger_name varchar (50) not null,
+    devoured boolean not null,
+    primary key (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+insert into burgers(burger_name) value ("Double Cheese Burger");
+insert into burgers(burger_name) value ("Delux Burger");
+insert into burgers(burger_name) value ("Bacon Burger");
